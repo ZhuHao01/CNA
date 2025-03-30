4684aef807e57072db4035189721695af6aa3205
Zhuhao1145247723@gmail.com <Zhuhao1145247723@gmail.com>
2025-03-30 21:46:29 +0800

    try-3

--- Proxy-skeleton(2025).py
+++ Proxy-skeleton(2025).py
@@ -122,6 +122,9 @@ while True:
     # ProxyServer finds a cache hit
     # Send back response to client 
     # ~~~~ INSERT CODE ~~~~
+    response = "".join(cacheData)
+    clientSocket.sendall(response.encode())                          #
+    
     # ~~~~ END CODE INSERT ~~~~
     cacheFile.close()
     print ('Sent to the client:')
@@ -132,6 +135,15 @@ while True:
     # Create a socket to connect to origin server
     # and store in originServerSocket
     # ~~~~ INSERT CODE ~~~~
+
+
+
+
+
+
+
+
+
     # ~~~~ END CODE INSERT ~~~~
 
     print ('Connecting to:\t\t' + hostname + '\n')
@@ -140,6 +152,15 @@ while True:
       address = socket.gethostbyname(hostname)
       # Connect to the origin server
       # ~~~~ INSERT CODE ~~~~
+
+
+
+
+
+
+
+
+
       # ~~~~ END CODE INSERT ~~~~
       print ('Connected to origin Server')
 
@@ -150,6 +171,19 @@ while True:
       # originServerRequest is the first line in the request and
       # originServerRequestHeader is the second line in the request
       # ~~~~ INSERT CODE ~~~~
+
+
+
+
+
+
+
+
+
+
+
+
+
       # ~~~~ END CODE INSERT ~~~~
 
       # Construct the request to send to the origin server
@@ -170,10 +204,30 @@ while True:
 
       # Get the response from the origin server
       # ~~~~ INSERT CODE ~~~~
+
+
+
+
+
+
+
+
+
+
       # ~~~~ END CODE INSERT ~~~~
 
       # Send the response to the client
       # ~~~~ INSERT CODE ~~~~
+
+
+
+
+
+
+
+
+
+
       # ~~~~ END CODE INSERT ~~~~
 
       # Create a new file in the cache for the requested file.
@@ -185,6 +239,17 @@ while True:
 
       # Save origin server response in the cache file
       # ~~~~ INSERT CODE ~~~~
+
+
+
+
+
+
+
+
+
+
+
       # ~~~~ END CODE INSERT ~~~~
       cacheFile.close()
       print ('cache file closed')
